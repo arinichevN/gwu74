4f10635cd4fca67497373d0077ca5bdccc6971c3
Arinichev Nikolay <arinichev_n@mail.ru>
2018-03-30 08:46:22 +0300

    c

--- device/common.c
+++ device/common.c
@@ -3,30 +3,31 @@
 #define PIN_QUERY_STR "select net_id, device_id, id_within_device, mode, pud, rsl, pwm_period_sec, pwm_period_nsec, secure_timeout_sec, secure_duty_cycle, secure_enable from pin limit %d"
 
 typedef struct {
-    DeviceList *list;
-} DeviceData;
-
-typedef struct {
     PinList *list;
     DeviceList *device_list;
 } PinData;
 
-FUN_LIST_INIT(Device)
-FUN_LIST_INIT(Pin)
-
 int getDevice_callback(void *data, int argc, char **argv, char **azColName) {
-    DeviceData *device_data = data;
+    DeviceList *list = data;
     int c = 0;
     for (int i = 0; i < argc; i++) {
         if (DB_COLUMN_IS("id")) {
-            device_data->list->item[device_data->list->length].id = atoi(argv[i]);
+            list->item[list->length].id = atoi(DB_COLUMN_VALUE);
             c++;
         } else if (DB_COLUMN_IS("i2c_addr")) {
-            device_data->list->item[device_data->list->length].i2c_addr = atoi(argv[i]);
-            // device_data->list->item[device_data->list->length].i2c_fd = I2COpen(device_data->i2c_path, atoi(argv[i]));
+            list->item[list->length].i2c_addr = atoi(DB_COLUMN_VALUE);
+            // list->item[list->length].i2c_fd = I2COpen(i2c_path, atoi(DB_COLUMN_VALUE));
             c++;
         } else if (DB_COLUMN_IS("i2c_path")) {
-            memcpy(device_data->list->item[device_data->list->length].i2c_path, argv[i], sizeof device_data->list->item[device_data->list->length].i2c_path);
+            size_t sz=0;
+            size_t sz1=strlen(DB_COLUMN_VALUE);
+            size_t sz2=sizeof list->item[list->length].i2c_path;
+            if(sz1 < sz2){
+                sz=sz1;
+            }else{
+                sz=sz2;
+            }
+            memcpy(list->item[list->length].i2c_path, DB_COLUMN_VALUE, sz);
             c++;
         } else {
 #ifdef MODE_DEBUG
@@ -35,7 +36,7 @@ int getDevice_callback(void *data, int argc, char **argv, char **azColName) {
             c++;
         }
     }
-    device_data->list->length++;
+    list->length++;
 #define N 3
     if (c != N) {
         fprintf(stderr, "%s(): required %d columns but %d found\n", F, N, c);
@@ -50,39 +51,39 @@ int getPin_callback(void *data, int argc, char **argv, char **azColName) {
     int c = 0;
     for (int i = 0; i < argc; i++) {
         if (DB_COLUMN_IS("net_id")) {
-            d->list->item[d->list->length].net_id = atoi(argv[i]);
+            d->list->item[d->list->length].net_id = atoi(DB_COLUMN_VALUE);
             c++;
         } else if (DB_COLUMN_IS("device_id")) {
-            d->list->item[d->list->length].device = getDeviceBy_id(atoi(argv[i]), d->device_list);
+            d->list->item[d->list->length].device = getDeviceBy_id(atoi(DB_COLUMN_VALUE), d->device_list);
             c++;
         } else if (DB_COLUMN_IS("id_within_device")) {
-            d->list->item[d->list->length].id_dev = atoi(argv[i]);
+            d->list->item[d->list->length].id_dev = atoi(DB_COLUMN_VALUE);
             c++;
         } else if (DB_COLUMN_IS("mode")) {
-            d->list->item[d->list->length].mode = getModeByStr(argv[i]);
+            d->list->item[d->list->length].mode = getModeByStr(DB_COLUMN_VALUE);
             c++;
         } else if (DB_COLUMN_IS("pud")) {
-            d->list->item[d->list->length].pud = getPUDByStr(argv[i]);
+            d->list->item[d->list->length].pud = getPUDByStr(DB_COLUMN_VALUE);
             c++;
         } else if (DB_COLUMN_IS("rsl")) {
-            d->list->item[d->list->length].pwm.rsl = atoi(argv[i]);
+            d->list->item[d->list->length].pwm.rsl = atoi(DB_COLUMN_VALUE);
             c++;
         } else if (DB_COLUMN_IS("pwm_period_sec")) {
-            d->list->item[d->list->length].pwm.period.tv_sec = atoi(argv[i]);
+            d->list->item[d->list->length].pwm.period.tv_sec = atoi(DB_COLUMN_VALUE);
             c++;
         } else if (DB_COLUMN_IS("pwm_period_nsec")) {
-            d->list->item[d->list->length].pwm.period.tv_nsec = atoi(argv[i]);
+            d->list->item[d->list->length].pwm.period.tv_nsec = atoi(DB_COLUMN_VALUE);
             c++;
         } else if (DB_COLUMN_IS("secure_timeout_sec")) {
-            d->list->item[d->list->length].secure_out.timeout.tv_sec = atoi(argv[i]);
+            d->list->item[d->list->length].secure_out.timeout.tv_sec = atoi(DB_COLUMN_VALUE);
             d->list->item[d->list->length].secure_out.timeout.tv_nsec = 0;
             d->list->item[d->list->length].secure_out.tmr.ready = 0;
             c++;
         } else if (DB_COLUMN_IS("secure_duty_cycle")) {
-            d->list->item[d->list->length].secure_out.duty_cycle = atoi(argv[i]);
+            d->list->item[d->list->length].secure_out.duty_cycle = atoi(DB_COLUMN_VALUE);
             c++;
         } else if (DB_COLUMN_IS("secure_enable")) {
-            d->list->item[d->list->length].secure_out.enable = atoi(argv[i]);
+            d->list->item[d->list->length].secure_out.enable = atoi(DB_COLUMN_VALUE);
             c++;
         } else {
 #ifdef MODE_DEBUG
@@ -121,25 +122,22 @@ int initDevPin(DeviceList *dl, PinList *pl, int device_count_max, int pin_count_
         sqlite3_close(db);
         return 0;
     }
-    if (!initDeviceList(dl, n)) {
+    RESIZE_M_LIST(dl, n);
+    if (dl->max_length != n) {
 #ifdef MODE_DEBUG
-        fprintf(stderr, "%s(): failed\n", F);
+        fprintf(stderr, "%s(): device_list resize failed\n", F);
 #endif
         return 0;
     }
-    memset(dl->item, 0, n * sizeof *(dl->item));
-    DeviceData data = {.list = dl};
+    NULL_LIST(dl);
     snprintf(q, sizeof q, "select id, i2c_path, i2c_addr from device limit %d", device_count_max);
-    if (!db_exec(db, q, getDevice_callback, &data)) {
-#ifdef MODE_DEBUG
-        fprintf(stderr, "%s(): failed\n", F);
-#endif
+    if (!db_exec(db, q, getDevice_callback, dl)) {
         sqlite3_close(db);
         return 0;
     }
-    if (dl->length != n) {
+    if (dl->length != dl->max_length) {
 #ifdef MODE_DEBUG
-        fprintf(stderr, "%s(): device found: %u, but expected: %d\n", F, dl->length, n);
+        fprintf(stderr, "%s(): device found: %u, but expected: %d\n", F, dl->length, dl->max_length);
 #endif
         sqlite3_close(db);
         return 0;
@@ -159,26 +157,24 @@ int initDevPin(DeviceList *dl, PinList *pl, int device_count_max, int pin_count_
         sqlite3_close(db);
         return 0;
     }
-    if (!initPinList(pl, n)) {
+    RESIZE_M_LIST(pl, n);
+    if (pl->max_length != n) {
 #ifdef MODE_DEBUG
-        fprintf(stderr, "%s(): failed\n", F);
+        fprintf(stderr, "%s(): pin_list resize failed\n", F);
 #endif
         return 0;
     }
-    memset(pl->item, 0, n * sizeof *(pl->item));
+    NULL_LIST(pl);
     PinData datap = {.list = pl, .device_list = dl};
     snprintf(q, sizeof q, PIN_QUERY_STR, pin_count_max);
     if (!db_exec(db, q, getPin_callback, &datap)) {
-#ifdef MODE_DEBUG
-        fprintf(stderr, "%s(): failed\n", F);
-#endif
         sqlite3_close(db);
         return 0;
     }
     sqlite3_close(db);
-    if (pl->length != n) {
+    if (pl->length != pl->max_length) {
 #ifdef MODE_DEBUG
-        fprintf(stderr, "%s(): pins found: %u, but expected: %d\n", F, dl->length, n);
+        fprintf(stderr, "%s(): pins found: %u, but expected: %d\n", F, pl->length, pl->max_length);
 #endif
         return 0;
     }
